ee53dcef28f0f813878938d2466516bbccb66732
yun <indl1670@gmail.com>
2023-08-01 15:15:29 +0900

    20230801-3

--- day01/src/day01/Main.java
+++ day01/src/day01/Main.java
@@ -3,7 +3,8 @@ package day01;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello");
+		System.out.print("Hello111");
+		System.out.print("Hello333");
 
 	}
 
