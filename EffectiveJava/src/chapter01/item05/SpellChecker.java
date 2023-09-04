2059d483a752aad054b73c5bcdcbfaa4dd574649
yun <indl1670@gmail.com>
2023-09-04 19:14:10 +0900

    의존성 객체 주입 예제

---
+++ EffectiveJava/src/chapter01/item05/SpellChecker.java
@@ -0,0 +1,20 @@
+package chapter01.item05;
+
+import java.util.List;
+
+public class SpellChecker {
+	// 자원 직접 명시
+	// SpellChecker 생성 시 Dictionary 무조건 생성되어야 함
+	// 유연하지 않음, 테스트에 적합하지 않음
+	private static final Dictionary dictionary = new Dictionary();
+	
+	private SpellChecker() {}
+	
+	public static boolean isValid(String word) {
+		return dictionary.contains(word);
+	}
+	
+	public static List<String> suggestions(String typo) {
+		return dictionary.closeWordsTo(typo);
+	}
+}
---
+++ EffectiveJava/src/chapter01/item05/SpellChecker2.java
@@ -0,0 +1,22 @@
+package chapter01.item05;
+
+import java.util.List;
+
+public class SpellChecker2 {
+	private final Dictionary dictionary;
+	
+	// 의존성 주입 생성
+	// Dictionary가 인터페이스 였을 때
+	// Dictionary가 바뀐다고 하더라도 모든 메소드 재사용 가능
+	public SpellChecker2(Dictionary dictionary) {
+		this.dictionary = dictionary;
+	}
+	
+	public static boolean isValid(String word) {
+		return dictionary.contains(word);
+	}
+	
+	public static List<String> suggestions(String typo) {
+		return dictionary.closeWordsTo(typo);
+	}
+}
